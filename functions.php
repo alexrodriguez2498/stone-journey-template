965231b97a8925eaed37b76955354dd4b4148650
luis <luiscarvajal2693@@gmail.com>
2020-02-04 11:55:08 -0500

    injected dependencies pro

--- functions.php
+++ functions.php
@@ -5,13 +5,13 @@
  */
 function theme_enqueue_scripts() {
         wp_enqueue_style( 'Font_Awesome', 'https://use.fontawesome.com/releases/v5.6.1/css/all.css' );
-        wp_enqueue_style( 'Bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css' );
-        wp_enqueue_style( 'MDB', get_template_directory_uri() . '/css/mdb.min.css' );
+        wp_enqueue_style( 'Bootstrap_css', get_template_directory_uri() . '/css/bootstrap.css' );
+        wp_enqueue_style( 'MDB', get_template_directory_uri() . '/css/mdb.css' );
         wp_enqueue_style( 'Style', get_template_directory_uri() . '/style.css' );
-        wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/js/jquery.min.js', array(), '3.4.1', true );
-        wp_enqueue_script( 'Tether', get_template_directory_uri() . '/js/popper.min.js', array(), '1.0.0', true );
-        wp_enqueue_script( 'Bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '1.0.0', true );
-        wp_enqueue_script( 'MDB', get_template_directory_uri() . '/js/mdb.min.js', array(), '1.0.0', true );
+        wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/js/jquery.js', array(), '3.4.1', true );
+        wp_enqueue_script( 'Tether', get_template_directory_uri() . '/js/popper.js', array(), '1.0.0', true );
+        wp_enqueue_script( 'Bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), '1.0.0', true );
+        wp_enqueue_script( 'MDB', get_template_directory_uri() . '/js/mdb.js', array(), '1.0.0', true );
 
         }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
