56b16dac46de89680f51cac6d18cd14e131763ed
alexrodriguez2498 <alexcarvajal2404@gmail.com>
2020-02-05 11:33:16 -0500

    salvame luis

--- inc/properties-detail/index.php
+++ inc/properties-detail/index.php
@@ -183,6 +183,182 @@
             font-weight: bold;
         }
 </style>
+<style>
+    td {
+        padding-top: 0.5rem !important;
+        padding-bottom: 0.5rem !important;
+        font-weight: bold !important;
+
+    }
+
+    table {
+        /*border: 1px solid #ded6d6;*/
+        margin-bottom: 0 !important;
+    }
+
+    h4 {
+        /*border: 1px solid #ded6d6;*/
+        padding: 2%;
+        margin-bottom: 0;
+    }
+
+    .small-text {
+        font-size: 0.8rem;
+        font-weight: bold;
+    }
+
+    .tittle-text {
+        font-size: 1rem;
+        font-weight: bold;
+    }
+
+    .navbar-padding {
+        padding-left: 4%;
+        padding-right: 4%;
+    }
+
+    .margin-rigth-min {
+        margin-right: 3%;
+        margin-top: 0.5rem;
+        margin-bottom: 0.5rem;
+        margin-left: 3%;
+    }
+
+    #contenedor {
+        /*margin: 30px 250px;*/
+        margin: auto;
+        display: flex;
+        justify-content: center;
+        margin-top: 2rem;
+        
+    }
+
+    #replica {
+        margin: auto;
+        display: flex;
+        justify-content: center;
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
+
+    .row_content {
+        background: #F9F5F1;
+        height: 50px;
+        align-items: center;
+        font-size: 0.8rem;
+        font-weight: bold;
+
+    }
+
+    #contenedor_derecho {
+        font-size: 0.8rem;
+        text-align: left;
+        line-height: 1.3rem;
+    }
+
+    .properties {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        margin-bottom: 15px;
+    }
+
+
+    .action_btn_table{
+        display:flex;
+        align-items: flex-end;    
+        justify-content: flex-end;
+        height: 70px;
+
+    }
+    .action_btn{
+        /* margin: 10px; */
+        width: 2rem;
+    }
+    #last_container{
+        /*margin: 20%;*/
+        display: flex;
+        justify-content: center ;
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
+    .imagen{
+        margin-right: 1rem;
+        margin-left: 1rem;
+    }
+    h3{
+        margin-bottom: 1.5rem;
+        margin-left: 1rem;
+    }
+    .user_edit{
+        background-color: white;
+        border-radius: 2rem;
+        position: absolute;
+        top: 60%;
+        right: 0%;
+    }
+    .column{
+        width:100%;
+        height: 100%;
+    }
+    .tittle-text{
+        margin-left: 1rem;
+    }
+
+    .card{
+        box-shadow: 0 0px 0px 0;
+    }
+
+    .container2 {
+        border-top: 1px solid #ded6d6;
+        border-bottom: 1px solid #ded6d6;
+        padding: 1rem 0;
+    }
+
+    .profile-content {
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+    }
+
+    b {
+        font-weight: bold;
+    }
+
+    .btn-custom {
+        border-radius: 2rem;
+        border: 1px solid #ded6d6;
+        box-shadow: 0 1px 2px 0;
+        background-color: white;
+        margin-right: 1rem;
+    }
+    .td_icon{
+        text-align:end;
+    }
+    .th_special{
+        padding-right:10rem;
+    }
+    .middle_container{
+        display:flex;
+        align-items:center;
+    }
+    .middle_container ul{
+        display:flex;
+        align-items:center;
+    }
+    .middle_container ul li{
+        margin:1rem 4rem 0 4rem;
+        align-items:center;
+        display:flex;
+    }
+    .middle_container ul li:nth-child(3){
+        border-bottom:1px solid black;
+    }
+    .end_container{
+        display:flex;
+        justify-content:flex-end;
+    }
+
+</style>
 
 
 <!--Navbar -->
@@ -242,50 +418,182 @@
 </nav>
 <!--/.Navbar -->
 
-<div class="row mt-3 container-box" >
-    <div class="mt-5 py-4 z-depth-1 col" id="background_1">
+<div class="container row" id="contenedor">
 
-    </div>
-    
+    <div class="container col-md-3 profile" >
+<!-- Card -->
+<div class="card">
+
+  <!-- Card image -->
+  <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" alt="Card image cap">
+
+  <!-- Card content -->
+  <div class="card-body">
+
+    <!-- Title -->
+    <h4 class="card-title"><a>Rossio 93</a></h4>
+    <!-- Text -->
+    <p class="card-text">Beauty in motion</p>
+
+  </div>
+
 </div>
-
-<!--<div class="container">-->
-    <div class="row mt-3 container-box">
-        <div class="py-3 z-depth-1 col container-child-left"id="background_2">
-            <section class="px-md-2 mx-md-2 dark-grey-text">
-
-                <h4>Properties</h4>
-
-            </section>
-        </div>
-
-        <div class=" py-3 z-depth-1 col container-child-right" id="background_3">
-            <section class="px-md-2 mx-md-2 dark-grey-text">
-
-                <h4>Clients&nbsp;&nbsp;&nbsp;</h4>
-
-            </section>
-        </div>
+<!-- Card -->
     </div>
-    <div class="row mt-3 container-box container_box_2">
-        <div class="py-3 z-depth-1 col container-child-left" id="background_4">
-            <section class="px-md-2 mx-md-2 dark-grey-text">
 
-                <h4>Milestones</h4>
+    <div class="container col-md-9" id="contenedor_derecho">
+        <div class="row row_content">
+            <div class="col ">
 
-            </section>
+                <i class="fas fa-building"></i>
+                PROPERTIES
         </div>
-
-        <div class=" py-3 z-depth-1 col container-child-right"id="background_5">
-            <section class="px-md-2 mx-md-2 dark-grey-text">
-
-                <h4>Documents</h4>
-
-            </section>
         </div>
+        <d class="row profile-content">
+            <div class="col-md-4 column">
+                <b>Name:</b> Kelly <br>
+                <b>Lastname:</b> Slater <br>
+                <b>Password:**********</b> <br>
+                <b>Telephone:</b> +351 9339948 <br>
+                <b>Nationality:</b> United States <br>
+                <b>Birthday:</b> 21/10 <br>
+                <b>Company:</b> Empire Holding
+
+            </div>
+            <div class="col-md-4 column">
+                <b>Properties:</b> Rossio, Vila Maria Pia <br>
+                <b>Account Manager:</b> Mariana Vozone <br>
+                <b>Comission:</b> <br>
+                <b>Discount:</b> Yes <br>
+                <!-- radio button "yes"-->
+<!--                <div class="custom-control custom-radio custom-control-inline">-->
+<!--                    <input type="radio" class="custom-control-input" id="defaultInline1"-->
+<!--                           name="inlineDefaultRadiosExample">-->
+<!--                    <label class="custom-control-label" for="defaultInline1">Yes</label>-->
+<!--                </div>-->
+<!---->
+                <!-- radio button "no"-->
+<!--                <div class="custom-control custom-radio custom-control-inline">-->
+<!--                    <input type="radio" class="custom-control-input" id="defaultInline2"-->
+<!--                           name="inlineDefaultRadiosExample">-->
+<!--                    <label class="custom-control-label" for="defaultInline2">No</label>-->
+<!--                </div>-->
+<!--                <br>-->
+
+                <b>Extra Works: </b> Yes <br>
+                <!-- radio button "yes"-->
+<!--                <div class="custom-control custom-radio custom-control-inline">-->
+<!--                    <input type="radio" class="custom-control-input" id="defaultInline1"-->
+<!--                           name="inlineDefaultRadiosExample">-->
+<!--                    <label class="custom-control-label" for="defaultInline1">Yes</label>-->
+<!--                </div>-->
+
+                <!-- radio button "no"-->
+<!--                <div class="custom-control custom-radio custom-control-inline">-->
+<!--                    <input type="radio" class="custom-control-input" id="defaultInline2"-->
+<!--                           name="inlineDefaultRadiosExample">-->
+<!--                    <label class="custom-control-label" for="defaultInline2">No</label>-->
+<!--                </div>-->
+<!--                <br>-->
+
+                <b>purpose: </b> Golden Visa <br>
+                <!-- radio button "investment"-->
+<!--                <div class="custom-control custom-radio custom-control-inline">-->
+<!--                    <input type="radio" class="custom-control-input" id="defaultInline1"-->
+<!--                           name="inlineDefaultRadiosExample">-->
+<!--                    <label class="custom-control-label" for="defaultInline1">Investment</label>-->
+<!--                </div>-->
+
+                <!-- radio button "Golden Visa"-->
+<!--                <div class="custom-control custom-radio custom-control-inline">-->
+<!--                    <input type="radio" class="custom-control-input" id="defaultInline2"-->
+<!--                           name="inlineDefaultRadiosExample">-->
+<!--                    <label class="custom-control-label" for="defaultInline2">Golden Visa</label>-->
+<!--                </div>-->
+
+                <!-- radio button "Live"-->
+<!--                <div class="custom-control custom-radio custom-control-inline">-->
+<!--                    <input type="radio" class="custom-control-input" id="defaultInline3"-->
+<!--                           name="inlineDefaultRadiosExample">-->
+<!--                    <label class="custom-control-label" for="defaultInline3">Live</label>-->
+<!--                </div>-->
+<!--                <br>-->
+
+            </div>
+
+            <div class="col-md-4 column">
+                <b>Legal Representative:</b> Marco Coelho <br>
+                <b>Email:</b> mcoelho@gmail.com <br>
+                <b>Phone:</b> +351 93399448 <br> <br>
+                <b>Agent Representative:</b> Heidi Coelho <br>
+                <b>Email:</b> mcoelho@gmail.com <br>
+                <b>Phone:</b> +351 93399448
+            </div>
     </div>
-<!--</div>-->
+</div>
+</div>
+<div class="container row_content middle_container">
+    <ul>
+        <li>Clients</li>
+        <li>Milestones</li>
+        <li>Documents</li>
+    </ul>
+</div>
+<div class="container" id="last_container">
+<table class="table">
+<tr>
+                    <th scope="col"><b></b></th>
+                    <th scope="col"><b>Name</b></th>
+                    <th scope th_special="col"><b>Owner</b></th>
+                    <th scope th_special="col"><b>Category</b></th>
+                    <th scope="col"><b>Date</b></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td class="td_icon"><i class="fas fa-file-invoice-dollar fa-2x"></i></td>
+                    <td>Payment Summons</td>
+                    <td>Private</td>
+                    <td>Financial</td>
+                    <td>20/02/2020</td>
+                </tr>
+                <tr>
+                    <td class="td_icon"><i class="fas fa-file-signature fa-2x"></i></td>
+                    <td>Promissory Contract Payment</td>
+                    <td>Private</td>
+                    <td>Legal</td>
+                    <td>20/02/2020</td>
+                </tr>
+                <tr>
+                    <td class="td_icon"><i class="fas fa-file-invoice fa-2x"></i></td>
+                    <td>Reservation Fee Reinforcement</td>
+                    <td>Private</td>
+                    <td>Financial</td>
+                    <td>20/02/2020</td>
+                </tr>
+                <tr>
+                    <td class="td_icon"><i class="fas fa-file-image fa-2x"></i></td>
+                    <td>Reservation Form</td>
+                    <td>Private</td>
+                    <td>Legal</td>
+                    <td>20/02/2020</td>
+                </tr>
+                <tr>
+                    <td class="td_icon"><i class="fas fa-images fa-2x"></i></td>
+                    <td>Welcome letter</td>
+                    <td>Private</td>
+                    <td>General</td>
+                    <td>20/02/2020</td>
+                </tr>
+                <thead>
+                </tbody>
+                <tr>
+                </div>
 
-
-
+                <!-- <div class="container end_container">
+                    <p>New document</p>
+                    <i class="fas fa-plus"></i>
+                </div> -->
+                alskdjlkasjdl
+  
 	<?php get_footer(); ?>
