4cb3568b1a74e71b24c5e19ce6c9d1b709dbaeba
luis <luiscarvajal2693@gmail.com>
2020-02-18 12:43:32 -0400

    merged con los cambios de alex

--- inc/properties/index.php
+++ inc/properties/index.php
@@ -26,7 +26,7 @@
     }
 
     body {
-        background-image: url("https://stonejstaging.wpengine.com/wp-content/uploads/2020/02/fondo.jpg");
+        background-image: url("https://stonejstaging.wpengine.com/wp-content/uploads/2020/02/P1_Quarto_white-scaled.jpg");
         background-size: cover;
         background-attachment: fixed;
         background-repeat: no-repeat;
