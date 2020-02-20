e30f92a1fd6e9f595f913a546bf6cb89b8bd8235
alex <alexcarvajal2404@gmail.com>
2020-02-19 20:34:52 -0400

    form

--- inc/properties-new/index.php
+++ inc/properties-new/index.php
@@ -16,6 +16,10 @@
         border-right: 2px solid #8c6f49 !important;
         border-bottom: 2px solid #8c6f49 !important;
     }
+    .footer-form{
+        padding-right:1rem;
+    }
+  
 
 </style>
 
@@ -98,8 +102,111 @@
                         <input type="checkbox" class="form-check-input" id="materialChecked4">
                         <label class="form-check-label" for="materialChecked4">Sold</label>
                     </div>
+                    <div class="md-form">
+                        <input type="text" id="name" class="form-control">
+                        <label for="name">Name</label>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <h5>Progression</h5>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="materialChecked5" checked>
+                        <label class="form-check-label" for="materialChecked5">[1]Promissory Contract</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="materialChecked6">
+                        <label class="form-check-label" for="materialChecked6">[2]Licence Construction</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="materialChecked7">
+                        <label class="form-check-label" for="materialChecked7">[3]Construction Completed</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="materialChecked8" checked>
+                        <label class="form-check-label" for="materialChecked8">[4]Habitation License</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="materialChecked9">
+                        <label class="form-check-label" for="materialChecked9">[5]Completion od Sale</label>
+                    </div>
+                   
+
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-4">
+                <select class="mdb-select md-form">
+                        <option value="" disabled selected>Number os Sellable Units</option>
+                        <option value="1">Option 1</option>
+                        <option value="2">Option 2</option>
+                        <option value="3">Option 3</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+            <div class="col-md-7">
+                <div class="md-form ">
+                        <input type="text" id="Estimated-date-for-begining-of-construction" class="form-control">
+                        <label for="Estimated-date-for-begining-of-construction">Estimated date for begining of construction</label>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                <div class="md-form ">
+                        <input type="text" id="Estimated-delivery-date" class="form-control">
+                        <label for="Estimated-delivery-date">Estimated delivery date</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 d-flex">
+                    <div class="footer-form">
+                        <h5>Main Image</h5>
+                        <button type="button" class="btn btn-default btn-sm"style="background:#947956 !important;width:8rem;">Upload</button>
+                    </div>
+                    <div class="md-form">
+                        <input type="text" id="name" class="form-control">
+                        <label for="name">Choose your file</label>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex">
+                    <div class="footer-form">
+                        <h5>Gallery Image</h5>
+                        <button type="button" class="btn btn-default btn-sm"style="background:#947956 !important;width:8rem;">Upload</button>
+                    </div>
+                    <div class="md-form">
+                        <input type="text" id="name" class="form-control">
+                        <label for="name">Choose your file</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 d-flex">
+                    <div class="footer-form">
+                        <h5>Logo</h5>
+                        <button type="button" class="btn btn-default btn-sm"style="background:#947956 !important;width:8rem;">Upload</button>
+                    </div>
+                    <div class="md-form">
+                        <input type="text" id="name" class="form-control">
+                        <label for="name">Choose your file</label>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex">
+                    <div class="footer-form">
+                        <h5>Documents</h5>
+                        <button type="button" class="btn btn-default btn-sm"style="background:#947956 !important;width:8rem;">Upload</button>
+                    </div>
+                    <div class="md-form">
+                        <input type="text" id="name" class="form-control">
+                        <label for="name">Choose your file</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col d-flex justify-content-end">
+                    <button type="button" class="btn btn-primary" style="background:#947956 !important;width:12rem;">Save</button>
+                </div>
+            </div>
+               
 
         </section>
         <!--Section: Content-->
