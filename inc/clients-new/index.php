d1d82e78b3acedac90a8afcf14a93479065fcc3d
alex <alexcarvajal2404@gmail.com>
2020-02-21 00:18:11 -0400

    asd

--- inc/clients-new/index.php
+++ inc/clients-new/index.php
@@ -24,10 +24,10 @@
     .footer-form{
         padding-right:1rem;
     }
-    .custom-control{
+    
+    .rbr, .formmiddle{
         padding-left:3rem;
     }
-   
   
 
 </style>
@@ -38,22 +38,71 @@
 
         <!--Section: Content-->
         <section class="px-md-2 mx-md-2 dark-grey-text" id="container__center" style="width: 100%;">
-
+        <h3 class="font-weight-bold">Add New Client</h3>
             <div class="row">
-                <div class="col-md-6 col-sm-12">
-                    <h3 class="font-weight-bold">Add New Property</h3>
-
+                    <div class="col-md-4 col-sm-12">
                     <div class="md-form">
                         <input type="text" id="name" class="form-control">
                         <label for="name">Name</label>
                     </div>
-
-                    <div class="md-form">
-                        <textarea id="form7" class="md-textarea form-control" rows="3"></textarea>
-                        <label for="form7">About Property</label>
                     </div>
 
-                    <select class="mdb-select md-form">
+                    <div class="col-md-4 col-sm-12">
+                    <div class="md-form">
+                        <input type="text" id="last-name" class="form-control">
+                        <label for="name">Last Name</label>
+                    </div>
+                    </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 d-flex align-items-center ">
+                    <h5 class="pr-4">Password</h5>
+                    <button type="button" class="btn btn-default btn-sm"style="background:#947956 !important;">GENERATE PASSWORD</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="md-form ">
+                        <input type="text" id="address" class="form-control">
+                        <label for="address">Address</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-3">
+                <select class="mdb-select md-form">
+                        <option value="" disabled selected>Nationality</option>
+                        <option value="1">Option 1</option>
+                        <option value="2">Option 2</option>
+                        <option value="3">Option 3</option>
+                    </select>
+                </div>
+                <div class="col-md-3 offset-md-1">
+                <div class="md-form">
+                        <input type="text" id="purpose" class="form-control">
+                        <label for="name">Purpose</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                <div class="md-form">
+                        <input type="text" id="telephone" class="form-control">
+                        <label for="name">Telephone</label>
+                    </div>
+                </div>
+                <div class="col-md-4 ">
+                <div class="md-form">
+                        <input type="text" id="birthday" class="form-control">
+                        <label for="name">Birthday</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                <select class="mdb-select md-form">
                         <option value="" disabled selected>City</option>
                         <option value="1">Option 1</option>
                         <option value="2">Option 2</option>
@@ -61,163 +110,142 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-6">
+                <div class="md-form">
+                        <input type="text" id="company" class="form-control">
+                        <label for="name">Company</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                <select class="mdb-select md-form">
+                        <option value="" disabled selected>Property</option>
+                        <option value="1">Option 1</option>
+                        <option value="2">Option 2</option>
+                        <option value="3">Option 3</option>
+                    </select>
+                </div>
+                <div class="col-md-2 offset-md-1 ">
+                <select class="mdb-select md-form">
+                        <option value="" disabled selected>Units</option>
+                        <option value="1">Option 1</option>
+                        <option value="2">Option 2</option>
+                        <option value="3">Option 3</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                <select class="mdb-select md-form">
+                        <option value="" disabled selected>Account Manager</option>
+                        <option value="1">Option 1</option>
+                        <option value="2">Option 2</option>
+                        <option value="3">Option 3</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <h5>Discount:</h5>
+                    <div class="radio-buttons d-flex align-items*center">
+                    <div class="custom-control custom-radio">
+                    <input type="radio" class="custom-control-input" id="defaultGroupExample9" name="groupOfDefaultRadiosRight">
+                    <label class="custom-control-label" for="defaultGroupExample9">Yes</label>
+                    </div>
+                    <select class="mdb-select md-form formmiddle">
+                        <option value="" disabled selected></option>
+                        <option value="1">Option 1</option>
+                        <option value="2">Option 2</option>
+                        <option value="3">Option 3</option>
+                    </select>
+                    <div class="custom-control custom-radio rbr">
+                    <input type="radio" class="custom-control-input" id="defaultGroupExample9" name="groupOfDefaultRadiosRight">
+                    <label class="custom-control-label" for="defaultGroupExample9">No</label>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2">
+                    <h5>Extra Works:</h5>
+                    <div class="radio-buttons d-flex align-items*center">
+                    <div class="custom-control custom-radio">
+                    <input type="radio" class="custom-control-input" id="defaultGroupExample9" name="groupOfDefaultRadiosRight">
+                    <label class="custom-control-label" for="defaultGroupExample9">Yes</label>
+                    </div>
+                   
+                    <div class="custom-control custom-radio rbr">
+                    <input type="radio" class="custom-control-input" id="defaultGroupExample9" name="groupOfDefaultRadiosRight">
+                    <label class="custom-control-label" for="defaultGroupExample9">No</label>
+                    </div>
+                    </div>
+                </div>
+            </div>
 
             <div class="row">
                 <div class="col-md-3">
-                    <select class="mdb-select md-form">
-                        <option value="" disabled selected>Foreseen Conclusion</option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                    </select>
-                </div>
-
-                <div class="col-md-4">
-                    <select class="mdb-select md-form">
-                        <option value="" disabled selected>Uses</option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                    </select>
-                </div>
-
-                <div class="col-md-6">
-                    <select class="mdb-select md-form">
-                        <option value="" disabled selected>Architect</option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                    </select>
-                </div>
-
-                <div class="col-12">
-                    <div class="md-form ">
-                        <input type="text" id="address" class="form-control">
-                        <label for="address">Address</label>
+                <div class="md-form">
+                        <input type="text" id="asking-price" class="form-control">
+                        <label for="name">Asking Price</label>
                     </div>
                 </div>
-
-                <div class="col-md-6">
-                    <h5>Properties Status</h5>
-                    <!-- Group of default radios - option 1 -->
-                    <div class="custom-control custom-radio">
-                    <input type="radio" class="custom-control-input" id="defaultGroupExample1" name="groupOfDefaultRadiosLeft">
-                    <label class="custom-control-label" for="defaultGroupExample1"> Completed</label>
+                <div class="col-md-3 ">
+                <div class="md-form">
+                        <input type="text" id="closing-price" class="form-control">
+                        <label for="name">Closing Price</label>
                     </div>
-
-                    <!-- Group of default radios - option 2 -->
-                    <div class="custom-control custom-radio">
-                    <input type="radio" class="custom-control-input" id="defaultGroupExample2" name="groupOfDefaultRadiosLeft" checked>
-                    <label class="custom-control-label" for="defaultGroupExample2">Current</label>
-                    </div>
-
-                    <!-- Group of default radios - option 3 -->
-                    <div class="custom-control custom-radio">
-                    <input type="radio" class="custom-control-input" id="defaultGroupExample3" name="groupOfDefaultRadiosLeft">
-                    <label class="custom-control-label" for="defaultGroupExample3">Sold</label>
-                    </div>
-    <!-- <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="materialChecked2" checked>
-        <label class="form-check-label" for="materialChecked2">Completed</label>
-    </div>
-    <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="materialChecked3">
-        <label class="form-check-label" for="materialChecked3">Current</label>
-    </div>
-    <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="materialChecked4">
-        <label class="form-check-label" for="materialChecked4">Sold</label>
-    </div> -->
-                    <div class="md-form">
-                        <input type="text" id="name" class="form-control">
-                        <label for="name">Website</label>
-                    </div>
-                </div>
-                <div class="col-md-5">
-                    <h5>Progression</h5>
-                    <!-- Group of default radios - option 1 -->
-                    <div class="custom-control custom-radio">
-                    <input type="radio" class="custom-control-input" id="defaultGroupExample4" name="groupOfDefaultRadiosRight">
-                    <label class="custom-control-label" for="defaultGroupExample4">[1]Promissory Contract</label>
-                    </div>
-
-                    <!-- Group of default radios - option 2 -->
-                    <div class="custom-control custom-radio">
-                    <input type="radio" class="custom-control-input" id="defaultGroupExample5" name="groupOfDefaultRadiosRight" checked>
-                    <label class="custom-control-label" for="defaultGroupExample5">[2]Licence Construction</label>
-                    </div>
-
-                    <!-- Group of default radios - option 3 -->
-                    <div class="custom-control custom-radio">
-                    <input type="radio" class="custom-control-input" id="defaultGroupExample6" name="groupOfDefaultRadiosRight">
-                    <label class="custom-control-label" for="defaultGroupExample6">[3]Construction Completed</label>
-                    </div><!-- Group of default radios - option 4 -->
-                    <div class="custom-control custom-radio">
-                    <input type="radio" class="custom-control-input" id="defaultGroupExample7" name="groupOfDefaultRadiosRight">
-                    <label class="custom-control-label" for="defaultGroupExample7">[4]Habitation License</label>
-                    </div>
-
-                    <!-- Group of default radios - option 5 -->
-                    <div class="custom-control custom-radio">
-                    <input type="radio" class="custom-control-input" id="defaultGroupExample8" name="groupOfDefaultRadiosRight" >
-                    <label class="custom-control-label" for="defaultGroupExample8">[5]Completion od Sale</label>
-                    </div>
-
-
-
-    <!-- <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="materialChecked5" checked>
-        <label class="form-check-label" for="materialChecked5">[1]Promissory Contract</label>
-    </div>
-    <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="materialChecked6">
-        <label class="form-check-label" for="materialChecked6">[2]Licence Construction</label>
-    </div>
-    <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="materialChecked7">
-        <label class="form-check-label" for="materialChecked7">[3]Construction Completed</label>
-    </div>
-    <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="materialChecked8" checked>
-        <label class="form-check-label" for="materialChecked8">[4]Habitation License</label>
-    </div>
-    <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="materialChecked9">
-        <label class="form-check-label" for="materialChecked9">[5]Completion od Sale</label>
-    </div> -->
-                   
-
                 </div>
             </div>
+            
             <div class="row">
-                <div class="col-md-4">
-                <select class="mdb-select md-form">
-                        <option value="" disabled selected>Number os Sellable Units</option>
-                        <option value="1">Option 1</option>
-                        <option value="2">Option 2</option>
-                        <option value="3">Option 3</option>
-                    </select>
-                </div>
-            </div>
-            <div class="row">
-            <div class="col-md-6">
-                <div class="md-form ">
-                        <input type="text" id="Estimated-date-for-begining-of-construction" class="form-control">
-                        <label for="Estimated-date-for-begining-of-construction">Estimated date for begining of construction</label>
+                <div class="col-md-6">
+                <div class="md-form">
+                        <input type="text" id="legal-representative" class="form-control">
+                        <label for="name">Legal Representative</label>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6">
-                <div class="md-form ">
-                        <input type="text" id="Estimated-delivery-date" class="form-control">
-                        <label for="Estimated-delivery-date">Estimated delivery date</label>
+                <div class="md-form">
+                        <input type="text" id="email" class="form-control">
+                        <label for="name">Email</label>
+                    </div>
+                </div>
+                <div class="col-md-3 ">
+                <div class="md-form">
+                        <input type="text" id="phone" class="form-control">
+                        <label for="name">Phone</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                <div class="md-form">
+                        <input type="text" id="agent-representative" class="form-control">
+                        <label for="name">Agent Representative</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                <div class="md-form">
+                        <input type="text" id="email2" class="form-control">
+                        <label for="name">Email</label>
+                    </div>
+                </div>
+                <div class="col-md-3 ">
+                <div class="md-form">
+                        <input type="text" id="phone2" class="form-control">
+                        <label for="name">Phone</label>
                     </div>
                 </div>
             </div>
        
             <div class="row">
-                <div class="col-md-6 d-flex">
+                <div class="col d-flex">
                     <div class="footer-form">
                         <h5>Main Image</h5>
                         <button type="button" class="btn btn-default btn-sm"style="background:#947956 !important;width:8rem;">Upload</button>
@@ -227,29 +255,10 @@
                         <label for="form10">Choose your file</label>
                     </div>
                 </div>
-                <div class="col-md-6 d-flex">
-                    <div class="footer-form">
-                        <h5>Gallery Image</h5>
-                        <button type="button" class="btn btn-default btn-sm"style="background:#947956 !important;width:8rem;">Upload</button>
-                    </div>
-                    <div class="md-form">
-                        <input type="text" id="form11" class="form-control">
-                        <label for="form11">Choose your file</label>
-                    </div>
-                </div>
+    
             </div>
             <div class="row">
-                <div class="col-md-6 d-flex">
-                    <div class="footer-form">
-                        <h5>Logo</h5>
-                        <button type="button" class="btn btn-default btn-sm"style="background:#947956 !important;width:8rem;">Upload</button>
-                    </div>
-                    <div class="md-form">
-                        <input type="text" id="form12" class="form-control">
-                        <label for="form12">Choose your file</label>
-                    </div>
-                </div>
-                <div class="col-md-6 d-flex">
+                <div class="col d-flex">
                     <div class="footer-form">
                         <h5>Documents</h5>
                         <button type="button" class="btn btn-default btn-sm"style="background:#947956 !important;width:8rem;">Upload</button>
